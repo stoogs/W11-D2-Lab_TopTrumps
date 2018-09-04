47c88f691154786447fc3e4946e9d78e31c5355a
Stuart Black <stuartblack@gmail.com>
2018-09-04 21:50:02 +0100

    Function created to pass value used for comparison to check function

--- game.js
+++ game.js
@@ -15,12 +15,50 @@ Game.prototype.checkAgility = function(p1cards){
     return p1cards[0].agility;
 }
 
-Game.prototype.checkValueOfCards = function(p1cards, p2cards){
+Game.prototype.checkValueOfCardsAgility = function(p1cards, p2cards){
     if (p2cards[0].agility > p2cards[0].agility){
         return "player 1 wins"
     } else {
         return "player 2 wins"
     }
+}
+
+Game.prototype.findWinnerAgility = function(p1cards, p2cards){
+    for (card1 of p1cards){
+        for (card2 of p2cards){
+            console.log(`player 1 ${card1.name} Agility is: ${card1.agility}`);
+            console.log(`player 2 ${card2.name} Agility is: ${card2.agility}`);
+            
+        if (card1.agility === card2.agility){
+            return "It's a Draw on Agility"
+    } else {
+            if (card1.agility > card2.agility){
+                return "player 1 wins on Agility"
+    } else {
+                return "player 2 wins on Agility"
+    }
+}
+}
+}
+}
+
+Game.prototype.findWinnerIntelligence = function(p1cards, p2cards){
+    for (card1 of p1cards){
+        for (card2 of p2cards){
+            console.log(`player 1 ${card1.name} intelligence is: ${card1.intelligence}`);
+            console.log(`player 2 ${card2.name} intelligence is: ${card2.intelligence}`);
+            
+        if (card1.intelligence === card2.intelligence){
+            return "It's a Draw on intelligence"
+    } else {
+            if (card1.intelligence > card2.intelligence){
+                return "player 1 wins on Intelligence"
+    } else {
+                return "player 2 wins on Intelligence"
+    }
+}
+}
+}
 }
 
 Game.prototype.givePlayersCards = function(){
@@ -31,9 +69,34 @@ Game.prototype.givePlayersCards = function(){
     this.player2.cards.push(this.cards[i+1]);
     i++;
 }
-    
 
+
+Game.prototype.findWinner = function(p1cards, p2cards, ability){
+    for (card1 of p1cards){
+        for (card2 of p2cards){
+            console.log(`player 1 ${card1.name} ${ability} is: ${card1[ability]}`);
+            console.log(`player 2 ${card2.name} ${ability} is: ${card2[ability]}`);
+        if (card1[ability] === card2[ability]){
+            return `It's a Draw on ${ability}`
+    } else {
+            if (card1[ability] > card2[ability]){
+                return `player 1 wins on ${ability}`
+    } else {
+                return `player 2 wins on ${ability}`
+    }
 }
+}
+}
+}
+
+
+
+
+
+
+
+
+}//end
 
 
 
--- specs/gameTest.js
+++ specs/gameTest.js
@@ -67,16 +67,65 @@ describe('Game', function(){
     it('Compare P1 P2 agility values, return winner in message', function(){
         game.giveCardToPlayer1(superman);
         game.giveCardToPlayer2(scarlet);
-        const actual = game.checkValueOfCards(player1.cards, player2.cards);
+        const actual = game.checkValueOfCardsAgility(player1.cards, player2.cards);
        assert.strictEqual(actual, "player 2 wins");
     });
 
     it('Deal cards to players', function(){
         game.givePlayersCards()
         const actual = game.player1.cards.length;    
-        console.log(game.player1.cards);    
-        console.log(game.player2.cards);    
+        const actual2 = game.player2.cards.length;    
+        // console.table(game.player1.cards);    
+        // console.table(game.player2.cards);    
         assert.strictEqual(actual, 3);
+        assert.strictEqual(actual2, 3);
+    });
+
+    // it('Check chosen attribute', function(){
+    //     game.givePlayersCards()
+    //     console.table(game.player1.cards);
+    //     console.table(game.player2.cards);
+    //     console.log("------------------------");
+    //     game.checkChosenAttribute();
+    //     console.table(game.player1.cards);
+    //     console.table(game.player2.cards);
+    //     assert.strictEqual(3, 3);
+    // });
+
+    it('Check Player 1 wins On Agility', function(){
+        game.givePlayersCards()
+        let actual = game.findWinnerAgility(player1.cards, player2.cards);
+        console.log(actual);
+        assert.strictEqual(actual, "player 1 wins on Agility");
+    });
+
+    it('Check Player 2 wins On Intelligence', function(){
+        game.givePlayersCards()
+        let actual = game.findWinnerIntelligence(player1.cards, player2.cards);
+        console.log(actual);
+        assert.strictEqual(actual, "player 2 wins on Intelligence");
+    });
+  
+    it('Check any passed property works', function(){
+        game.givePlayersCards()
+        let actual = game.findWinner(player1.cards, player2.cards, 'strength' );
+        assert.strictEqual(actual, "player 2 wins on strength");
+        console.log(actual)
+    });
+
+    it('Check any passed property works', function(){
+        game.givePlayersCards()
+       
+        let actual2 = game.findWinner(player1.cards, player2.cards, 'intelligence' );
+        assert.strictEqual(actual2, "player 2 wins on intelligence");
+        console.log(actual2)
+    });
+
+    it('Check any passed property works', function(){
+        game.givePlayersCards()
+        let actual3 = game.findWinner(player1.cards, player2.cards, 'agility' );
+        assert.strictEqual(actual3, "player 1 wins on agility");
+        console.log(actual3)
     });
 
 });
